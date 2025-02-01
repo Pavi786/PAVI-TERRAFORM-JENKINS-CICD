cdebae02d3065f10745241f9e67c466994b0bc53
Pavithran <132563862+Pavi786@users.noreply.github.com>
2025-02-01 14:01:00 +0530

    Update backend.tf
    terraform

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "my-statcic-website "
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "my-pavi-jilla"
   }
 }
